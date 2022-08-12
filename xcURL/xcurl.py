cc0f98e4ab7faf4bd4e74b69ce9251dcab011625
Plogoman <54278257+Plogoman@users.noreply.github.com>
2022-08-12 05:21:38 +0200

    Add files via upload

--- xcURL/xcurl.py
+++ xcURL/xcurl.py
@@ -1,11 +1,15 @@
 import click as c
-from pywebcopy import save_website as save
+from pywebcopy import save_website, save_webpage
 
-@c.command()
+@c.group()
+def url():
+    pass
+
+@c.command(name="website")
 @c.option('--url', '-u', help="The URL to download", required=True)
 @c.option('--folder', '-f', help="The Folder to download the website into", required=True)
-def url(url, folder):
-    save(
+def save_all_website(url, folder):
+    save_website(
         url=url,
         project_folder=folder,
         bypass_robots=True,
@@ -14,3 +18,20 @@ def url(url, folder):
         delay=None,
         threaded=False,
     )
+
+@c.command(name="webpage")
+@c.option('--url', '-u', help="The URL to download", required=True)
+@c.option('--folder', '-f', help="The Folder to download the website into", required=True)
+def save_only_webpage(url, folder):
+    save_webpage(
+        url=url,
+        project_folder=folder,
+        bypass_robots=True,
+        debug=True,
+        open_in_browser=True,
+        delay=None,
+        threaded=False,
+    )
+
+url.add_command(save_only_webpage)
+url.add_command(save_all_website)
